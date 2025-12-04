502795b8177caecab2acd1fa42acc6b39ede0080
Pedro Rolin Schmitz <93809401+PedroRSchmitz@users.noreply.github.com>
2025-12-04 19:29:19 -0300

    Delete Estrutura/Ocean directory

--- Estrutura/Ocean/database/add_subject_field.sql
+++
@@ -1,6 +0,0 @@
--- Adicionar campo de disciplina/matéria nas tarefas
-ALTER TABLE tasks ADD COLUMN subject VARCHAR(100) AFTER category_id;
-
--- Adicionar índice para melhorar performance de ordenação
-CREATE INDEX idx_tasks_subject ON tasks(subject);
-CREATE INDEX idx_tasks_due_date ON tasks(due_date);
